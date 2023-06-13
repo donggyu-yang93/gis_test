125e3032b3bc63cc56242c1342cd2582f02f4d6a
Mr.Yang <ydg0614@gmail.com>
2023-06-13 15:13:20 +0900

    0613 카카오지도

--- gis_test/blog/models.py
+++ gis_test/blog/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Post(models.Model):
     farm_num = models.TextField(max_length=100, blank=True, null=True)
     farm_name = models.CharField(max_length=100, null=True, default='None')
-    lat = models.TextField(max_length=100, blank=True, null=True)
-    long = models.TextField(max_length=100, blank=True, null=True)
-    distance = models.TextField(max_length=100, blank=True, null=True)
-    result = models.TextField(max_length=100, blank=True, null=True)
+    lat = models.FloatField(max_length=100, blank=True, null=True)
+    long = models.FloatField(max_length=100, blank=True, null=True)
+    distance = models.FloatField(max_length=100, blank=True, null=True)
+    result = models.FloatField(max_length=100, blank=True, null=True)
 
-# Create your models here.
+
--- gis_test/blog/views.py
+++ gis_test/blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
+from django.core import serializers
+from django.http import JsonResponse
 
-# Create your views here.
+
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+    paginate_by = 12
+
+    def render_to_response(self, context, **response_kwargs):
+        posts = list(self.model.objects.values('lat', 'long'))  # 작업 필터를 적용하고 목록으로 변환
+        return JsonResponse({'positions': posts}, safe=False)  # safe=False를 사용하여 비사전 객체도 JSON으로 전달
