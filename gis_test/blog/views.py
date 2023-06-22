f959533f1f0f9cc4ea0f3e7e0bed51cb7970aa99
Mr.Yang <ydg0614@gmail.com>
2023-06-22 23:11:43 +0900

    0618 수정

--- gis_test/blog/views.py
+++ gis_test/blog/views.py
@@ -9,10 +9,9 @@ from django.http import JsonResponse
 class PostList(ListView):
     model = Post
     ordering = '-pk'
-    paginate_by = 12
 
     # def render_to_response(self, context, **response_kwargs):
-    #     posts = list(self.model.objects.values('lat', 'long'))  # 작업 필터를 적용하고 목록으로 변환
+    #     posts = list(self.model.objects.values('farm_num','farm_name','lat', 'long','distance','result'))  # 작업 필터를 적용하고 목록으로 변환
     #     return JsonResponse({'positions': posts}, safe=False)
 
 def get_info(request):
